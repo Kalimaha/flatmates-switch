47b6054fb3561223ae0eff9b1867769b511134e9
Jay Jamero <jay.jamero@rea-group.com>
2018-11-16 08:21:05 +1100

    Layout updates

--- lib/switch_web/client/js/layouts/HeaderLayout/index.jsx
+++ lib/switch_web/client/js/layouts/HeaderLayout/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styles from './styles.scss'
+import React from "react";
+import styles from "./styles.scss";
 
 function HeaderLayout() {
   const BROWSE_HAPPY = `<!--[if lte IE 8]>
@@ -7,15 +7,15 @@ function HeaderLayout() {
       You are using an <strong>outdated</strong> browser.
       Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.
     </p>
-  <![endif]-->`
+  <![endif]-->`;
   return (
     <header className={styles.header}>
       <div dangerouslySetInnerHTML={{ __html: BROWSE_HAPPY }} />
       <div className="l-pg l-pg--width">
-        <h1>React Redux Starter</h1>
+        <h1>Switchboard</h1>
       </div>
     </header>
-  )
+  );
 }
 
-export default HeaderLayout
+export default HeaderLayout;
